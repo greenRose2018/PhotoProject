a897f20468b1b9e5108aa72cf2991f5d36d3af4f
greenRose2018 <brifun13@gmail.com>
2018-01-30 08:53:39 -0700

    created method mirrorHorizontal()

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -119,6 +119,23 @@ public class Picture extends SimplePicture
     } 
   }
   
+  public void mirrorHorizontal()
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	  Pixel topPixel = null;
+	  Pixel bottomPixel = null;
+	  int height = pixels.length;
+	  for(int col = 0; col < pixels[0].length; col++)
+	  {
+		  for (int row = 0; row < height/2; row++)
+		  {
+			  topPixel = pixels[row][col];
+			  bottomPixel = pixels[height -1 - row][col];
+			  topPixel.setColor(bottomPixel.getColor());
+		  }
+	  }
+  }
+  
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
   {
